7735cefa68ee8e13981885656e3cec188365558a
kschreiblehner <Ff77cd5f6>
2021-11-29 09:20:01 +0100

    initial example

--- css_hover_and_release_reactjs/src/components/HoverableLink.js
+++ css_hover_and_release_reactjs/src/components/HoverableLink.js
@@ -1,7 +1,38 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './HoverableLink.scss';
 
 export default function HoverableLink({ href, children }) {
-  console.log(children)
-  return <Link to={href} className="link">{children}</Link>;
+  const [hoverAnimationStarted, setHoverAnimationStarted] = useState();
+  const [leaveAnimationStarted, setLeaveAnimationStarted] = useState();
+
+  function startHoverAnimation() {
+    setHoverAnimationStarted(true);
+  }
+
+  function startLeaveAnimation() {
+    setHoverAnimationStarted(false);
+    setLeaveAnimationStarted(true);
+  }
+
+  function animate() {
+    if (hoverAnimationStarted) {
+      return 'hoverAnimation';
+    }
+    if (leaveAnimationStarted) {
+      return 'leaveAnimation';
+    }
+  }
+
+  return (
+    <Link
+      to={href}
+      className="link"
+      onMouseEnter={startHoverAnimation}
+      onMouseLeave={startLeaveAnimation}
+    >
+      {children}
+      <div className={`helper ${animate()}`}></div>
+    </Link>
+  );
 }
